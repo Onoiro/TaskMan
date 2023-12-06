2576ce965ecbb9d807722002e22429bd9177b85f
Andrey Bogatyrev <t2way@yandex.ru>
2023-12-07 00:51:21 +0300

    update views.py: add import gettext & pass vars for top menu through gettext

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.utils.translation import gettext
 
 
 def index(request):
-    return render(request, 'index.html', context={'who': 'World'})
+    task_manager = gettext("Task manager")
+    users = gettext("Users")
+    statuses = gettext("Statuses")
+    labels = gettext("Labels")
+    tasks = gettext("Tasks")
+    return render(request, 'index.html',
+                  context={'task_manager': task_manager,
+                           'users': users,
+                           'statuses': statuses,
+                           'labels': labels,
+                           'tasks': tasks
+                           })
