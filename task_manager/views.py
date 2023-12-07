8e7f0f3a3c0132a52076df39c74de31f35ecf31f
Andrey Bogatyrev <t2way@yandex.ru>
2023-12-07 15:20:51 +0300

    update views.py: add ... import gettext as _

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.utils.translation import gettext
+from django.utils.translation import gettext as _
 
 
 def index(request):
-    task_manager = gettext("Task manager")
-    users = gettext("Users")
-    statuses = gettext("Statuses")
-    labels = gettext("Labels")
-    tasks = gettext("Tasks")
+    task_manager = _("Task manager")
+    users = _("Users")
+    statuses = _("Statuses")
+    labels = _("Labels")
+    tasks = _("Tasks")
     return render(request, 'index.html',
                   context={'task_manager': task_manager,
                            'users': users,
