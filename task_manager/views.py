992b68afda166904d77c2f18c5243be50d1eef0c
Andrey Bogatyrev <t2way@yandex.ru>
2023-12-09 20:28:44 +0300

    add new translate strings so update views.py, index.html, locale/ru.../django.po&mo

--- task_manager/views.py
+++ task_manager/views.py
@@ -8,10 +8,18 @@ def index(request):
     statuses = _("Statuses")
     labels = _("Labels")
     tasks = _("Tasks")
+    exit = _("Exit")
+    hello_from_hexlet = _("Hello from Hexlet!")
+    coding_courses = _('Practical programming courses')
+    read_more = _("Read more")
     return render(request, 'index.html',
                   context={'task_manager': task_manager,
                            'users': users,
                            'statuses': statuses,
                            'labels': labels,
-                           'tasks': tasks
+                           'tasks': tasks,
+                           'exit': exit,
+                           'hello_from_hexlet': hello_from_hexlet,
+                           'coding_courses': coding_courses,
+                           'read_more': read_more,
                            })
