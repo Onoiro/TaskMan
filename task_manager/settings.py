033a89fdbb9123cc4031efeff2267fb8873ae464
Andrey Bogatyrev <t2way@yandex.ru>
2023-12-12 16:55:43 +0300

    add new app user: - update settings.py INSTALLED_APPS  with add new app - add task_manager/user with standart modules include urls.py - add templates/user with index.html

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_bootstrap5',
     'task_manager',
+    'task_manager.user',
 ]
 
 MIDDLEWARE = [
---
+++ task_manager/user/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.utils.translation import gettext as _
+
+
+def index(request):
+    users = _("Users")
+    return render(request, 'index.html',
+                  context={'users': users,
+                           })
+
