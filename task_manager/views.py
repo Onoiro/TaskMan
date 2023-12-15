6fa81b93c412a638cd052654eacc6b5e4c86eca3
Andrey Bogatyrev <t2way@yandex.ru>
2023-12-15 21:33:14 +0300

    update views.py: remove all data that translates in base.html template & make django-admin compilemessages

--- task_manager/views.py
+++ task_manager/views.py
@@ -3,23 +3,11 @@ from django.utils.translation import gettext as _
 
 
 def index(request):
-    task_manager = _("Task manager")
-    users = _("Users")
-    statuses = _("Statuses")
-    labels = _("Labels")
-    tasks = _("Tasks")
-    exit = _("Exit")
     hello_from_hexlet = _("Hello from Hexlet!")
     coding_courses = _('Practical programming courses')
     read_more = _("Read more")
     return render(request, 'index.html',
-                  context={'task_manager': task_manager,
-                           'users': users,
-                           'statuses': statuses,
-                           'labels': labels,
-                           'tasks': tasks,
-                           'exit': exit,
-                           'hello_from_hexlet': hello_from_hexlet,
+                  context={'hello_from_hexlet': hello_from_hexlet,
                            'coding_courses': coding_courses,
                            'read_more': read_more,
                            })
