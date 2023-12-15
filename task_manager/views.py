75c72db8c7a7347473224ae24c43abe280f2d704
Andrey Bogatyrev <t2way@yandex.ru>
2023-12-16 00:17:51 +0300

    correct base.html: back to value='{{ exit }} so correct views.py & django.mo

--- task_manager/views.py
+++ task_manager/views.py
@@ -6,8 +6,10 @@ def index(request):
     hello_from_hexlet = _("Hello from Hexlet!")
     coding_courses = _('Practical programming courses')
     read_more = _("Read more")
+    exit = _("Exit")
     return render(request, 'index.html',
                   context={'hello_from_hexlet': hello_from_hexlet,
                            'coding_courses': coding_courses,
                            'read_more': read_more,
+                           'exit': exit,
                            })
