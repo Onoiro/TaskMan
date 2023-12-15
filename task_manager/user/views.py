0187ecb5d2338e42078b06620989ccd7168a79f1
Andrey Bogatyrev <t2way@yandex.ru>
2023-12-16 00:35:52 +0300

    update user/views.py with pass 'Exit'

--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -4,7 +4,9 @@ from django.utils.translation import gettext as _
 
 def index(request):
     users = _("Users")
+    exit = _("Exit")
     return render(request, 'user/index.html',
                   context={'users': users,
+                           'exit': exit,
                            })
 
