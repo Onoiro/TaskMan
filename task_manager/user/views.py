f8fe9b0d1af1fbedabe91f715b804ffd5b6a7b92
Andrey Bogatyrev <t2way@yandex.ru>
2023-12-16 16:00:22 +0300

    update all urls.py&views.py: change view functions to class based views

--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 from django.utils.translation import gettext as _
 
 
-def index(request):
-    users = _("Users")
-    exit = _("Exit")
-    return render(request, 'user/index.html',
-                  context={'users': users,
-                           'exit': exit,
-                           })
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        users = _("Users")
+        exit = _("Exit")
+        return render(request, 'user/index.html',
+                    context={'users': users,
+                            'exit': exit,
+                            })
 
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
+from django.views import View
 from django.utils.translation import gettext as _
 
 
-def index(request):
-    hello_from_hexlet = _("Hello from Hexlet!")
-    coding_courses = _('Practical programming courses')
-    read_more = _("Read more")
-    exit = _("Exit")
-    return render(request, 'index.html',
-                  context={'hello_from_hexlet': hello_from_hexlet,
-                           'coding_courses': coding_courses,
-                           'read_more': read_more,
-                           'exit': exit,
-                           })
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        hello_from_hexlet = _("Hello from Hexlet!")
+        coding_courses = _('Practical programming courses')
+        read_more = _("Read more")
+        exit = _("Exit")
+        return render(request, 'index.html',
+                    context={'hello_from_hexlet': hello_from_hexlet,
+                            'coding_courses': coding_courses,
+                            'read_more': read_more,
+                            'exit': exit,
+                            })
