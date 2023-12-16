594d8f57d5853d11e40697e395bb493316fa3339
Andrey Bogatyrev <t2way@yandex.ru>
2023-12-16 18:55:31 +0300

    update user/views.py & user/index.html - get users from db.sqlit3 & pass it to template - make navbar & make list of users by {% for user in users %} and so"

--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from .models import User
 from django.utils.translation import gettext as _
 
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        users = _("Users")
+        Users = _("Users")
+        users = User.objects.all()
         exit = _("Exit")
         return render(request, 'user/index.html',
-                    context={'users': users,
+                    context={'users': Users,
+                             'users': users,
                             'exit': exit,
                             })
-
