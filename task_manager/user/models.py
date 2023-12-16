252c6336cd11ac4a5cffcea52c677f040343ca44
Andrey Bogatyrev <t2way@yandex.ru>
2023-12-16 18:24:13 +0300

    correct models.py: correct 'id' instead 'ID' in class User

--- task_manager/user/models.py
+++ task_manager/user/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    ID = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     user_name = models.CharField(max_length=100)
     fullname = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
