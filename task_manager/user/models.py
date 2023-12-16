9f7a39792f446486be1973af61c51c59eb8a2c30
Andrey Bogatyrev <t2way@yandex.ru>
2023-12-16 17:02:46 +0300

    correct name to user_name in class User models.py

--- task_manager/user/models.py
+++ task_manager/user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     ID = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=100)
     fullname = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
