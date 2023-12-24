440107cb1bd0fd2c440875b72c7ca64167534b64
Andrey Bogatyrev <t2way@yandex.ru>
2023-12-24 23:07:15 +0300

    modify user/models.py - rename user_name to username - remove fullname and add first_name & last_name - add password - make migrations & make migrate - correct user/user_list.html with union {{ user.first_name}} {{ user.last_name }} instead {{ fullname }}

--- task_manager/user/models.py
+++ task_manager/user/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, \
+                                   RegexValidator
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
-    user_name = models.CharField(max_length=100)
-    fullname = models.CharField(max_length=200)
+    username = models.CharField(max_length=150, unique=True, blank=False,
+                                validators=[RegexValidator(
+                                '^[a-zA-Z0-9.@_+-]+$')])
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    password = models.CharField(max_length=100, blank=False,
+                                validators=[MinLengthValidator(3)]
+                                )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
