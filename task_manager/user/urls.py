460d327b815d57c1ebafa12d621ab607b33448a4
Andrey Bogatyrev <t2way@yandex.ru>
2024-01-02 16:14:59 +0300

    add UserUpdateView, UserDeleteView and return user_create_form.html like was last time before - user/views.py: add UpdateView, DeleteView & import LoginRequiredMixin, UserPassesTestMixin - user/urls.py: add path 'int:pk/edit/', 'int:pk/delete/' - user_create_form.html: add {% load django_bootstrap5 %}

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserList, UserCreate
+from .views import UserListView, UserCreateView, UserUpdateView
 
 urlpatterns = [
-    path('', UserList.as_view(), name='user-list'),
-    path('create/', UserCreate.as_view(), name='user-create'),
+    path('', UserListView.as_view(), name='user-list'),
+    path('create/', UserCreateView.as_view(), name='user-create'),
+    path('int:pk/edit/', UserUpdateView.as_view, name='user-update'),
+    path('int:pk/delete/', UserUpdateView.as_view, name='user-delete'),
 ]
--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import redirect
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import User
 from .forms import UserForm
 from django.utils.translation import gettext as _
 from django.urls import reverse_lazy
 
 
-class UserList(ListView):
+class UserListView(ListView):
     model = User
     form_class = UserForm()
     context_object_name = 'users_list'
     template_name = 'user_list.html'
 
 
-class UserCreate(CreateView):
+class UserCreateView(CreateView):
     model = User
     form_class = UserForm
     template_name = 'user/user_create_form.html'
@@ -28,3 +29,24 @@ class UserCreate(CreateView):
                 return redirect(self.success_url)
             else:
                 form = UserForm()
+
+
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = 'user/user_update.html'
+    success_url = 'user-list'
+
+    def test_func(self):
+        user = self.get_object()
+        return self.request.user == user
+
+
+class UserDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = User
+    template_name = 'user/user_delete.html'
+    success_url = 'user-list'
+
+    def test_func(self):
+        user = self.get_object()
+        return self.request.user == user
