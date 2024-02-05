7afe85a76bc2b906133ee20ca01795691527b44d
Andrey Bogatyrev <t2way@yandex.ru>
2024-02-05 15:36:25 +0300

    make refactor user/views.py with add class UserPermissions so cut UserUpdateView & UserDeleteView

--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -23,7 +23,18 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     success_message = 'User created successfully'
 
 
-class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+class UserPermissions(LoginRequiredMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.error(request, 'You are not authorized! Please login.')
+            return super().dispatch(request, *args, **kwargs)
+        if not self.get_object() == self.request.user:
+            messages.error(request, "You don't have permissions to modify another user.")
+            return redirect('user:user-list')
+        return super().dispatch(request, *args, **kwargs)
+
+
+class UserUpdateView(UserPermissions, SuccessMessageMixin, UpdateView):
     model = User
     form_class = UserRegisterForm
     template_name = 'user/user_update.html'
@@ -32,29 +43,11 @@ class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     success_url = reverse_lazy('user:user-list')
     success_message = 'User updated successfully'
 
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            messages.error(request, 'You are not authorized! Please login.')
-            return super().dispatch(request, *args, **kwargs)
-        if not self.get_object() == self.request.user:
-            messages.error(request, "You don't have permissions to modify another user.")
-            return redirect('user:user-list')
-        return super().dispatch(request, *args, **kwargs)
 
-
-class UserDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+class UserDeleteView(UserPermissions, SuccessMessageMixin, DeleteView):
     model = User
     template_name = 'user/user_delete.html'
     login_url = 'login'
     redirect_field_name = "redirect_to"
     success_url = reverse_lazy('user:user-list')
     success_message = 'User deleted successfully'
-
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            messages.error(request, 'You are not authorized! Please login.')
-            return super().dispatch(request, *args, **kwargs)
-        if not self.get_object() == self.request.user:
-            messages.error(request, "You don't have permissions to modify another user.")
-            return redirect('user:user-list')
-        return super().dispatch(request, *args, **kwargs)
