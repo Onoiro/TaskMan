6165e0be4b8e0d3f978b00171f2306ea43fefe17
Andrey Bogatyrev <t2way@yandex.ru>
2024-02-05 16:08:54 +0300

    make all texts and notifications ready for 118n

--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,6 +1,7 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.utils.translation import gettext as _
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -20,16 +21,16 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     form_class = UserRegisterForm
     template_name = 'user/user_create_form.html'
     success_url = reverse_lazy('login')
-    success_message = 'User created successfully'
+    success_message = _('User created successfully')
 
 
 class UserPermissions(LoginRequiredMixin):
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            messages.error(request, 'You are not authorized! Please login.')
+            messages.error(request, _('You are not authorized! Please login.'))
             return super().dispatch(request, *args, **kwargs)
         if not self.get_object() == self.request.user:
-            messages.error(request, "You don't have permissions to modify another user.")
+            messages.error(request, _("You don't have permissions to modify another user."))
             return redirect('user:user-list')
         return super().dispatch(request, *args, **kwargs)
 
@@ -41,7 +42,7 @@ class UserUpdateView(UserPermissions, SuccessMessageMixin, UpdateView):
     login_url = 'login'
     redirect_field_name = "redirect_to"
     success_url = reverse_lazy('user:user-list')
-    success_message = 'User updated successfully'
+    success_message = _('User updated successfully')
 
 
 class UserDeleteView(UserPermissions, SuccessMessageMixin, DeleteView):
@@ -50,4 +51,4 @@ class UserDeleteView(UserPermissions, SuccessMessageMixin, DeleteView):
     login_url = 'login'
     redirect_field_name = "redirect_to"
     success_url = reverse_lazy('user:user-list')
-    success_message = 'User deleted successfully'
+    success_message = _('User deleted successfully')
