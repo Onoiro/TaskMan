4d171aadc40d1eae7880a698b76a165c136a7cd1
Andrey Bogatyrev <t2way@yandex.ru>
2024-02-22 19:40:01 +0300

    add first test for create_users but test is fail & create fixture users.json

--- task_manager/user/tests.py
+++ task_manager/user/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+# from django.core.management import call_command
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.c = Client()
+        # call_command('loaddata', 'users.json')
+        self.user_data = {
+            'first_name': 'test_first_name',
+            'last_name': 'test_last_name',
+            'username': 'test_username',
+            'password': 'test_password',
+        }
+
+    def test_create_user(self):
+        response = self.c.post(('user-create', self.user_data))
+        print(self.user_data['username'])
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(response.status_code, 200)
+        
+
+
+
+
+# from django.contrib.auth.models import User
+# from django.test import Client
+# from django.core.management import call_command
+
+
+# class UserTestCase(TestCase):
+# def setUp(self):
+# c = Client()
+# call_command('loaddata', 'users.json')
+# user_data = {
+#     'first_name': 'test_first_name',
+#     'last_name': 'test_last_name',
+#     'username': 'test_username',
+#     'password': 'test_password',
+# }
+
+# def test_create_user(self):
+#     response = c.post(('user-create', user_data))
+#     self.assertEqual(response.status_code, 200)
+#     self.assertEqual(User.objects.count(), 1)
+#     user = User.objects.filter(username=self.c['username']).first()
+#     self.assertEqual(user.first_name, self.c.user_data['first_name'])
