0e20663f26348821422e67fca7c18798aaff51de
Andrey Bogatyrev <t2way@yandex.ru>
2024-02-24 22:41:18 +0300

    clear some in test_users.py but still only response.status_code is 200

--- tests/test_users.py
+++ tests/test_users.py
@@ -12,7 +12,7 @@ from django.core.management import call_command
 
 class UserTestCase(TestCase):
 
-    fixtures = ['new_test_user.json']
+    # fixtures = ['new_test_user.json']
 
     # @classmethod
     # def setUpTestData(cls):
@@ -20,8 +20,13 @@ class UserTestCase(TestCase):
 
     def setUp(self):
         self.c = Client()
-        call_command('loaddata', 'new_test_user.json')
-        
+        # call_command('loaddata', 'new_test_user.json')
+        self.user_data = {
+                    'first_name': 'test_first_name',
+                    'last_name': 'test_last_name',
+                    'username': 'test_username',
+                    'password': 'test_password',
+            }
 
     def test_create_user(self):
         old_count = User.objects.count()
@@ -33,29 +38,3 @@ class UserTestCase(TestCase):
         # self.assertIsNotNone(user)
         # self.assertEqual(user.first_name, self.user_data['first_name'])
         # self.assertEqual(user.last_name, self.user_data['last_name'])
-
-
-
-
-# from django.contrib.auth.models import User
-# from django.test import Client
-# from django.core.management import call_command
-
-
-# class UserTestCase(TestCase):
-# def setUp(self):
-# c = Client()
-# call_command('loaddata', 'users.json')
-# user_data = {
-#     'first_name': 'test_first_name',
-#     'last_name': 'test_last_name',
-#     'username': 'test_username',
-#     'password': 'test_password',
-# }
-
-# def test_create_user(self):
-#     response = c.post(('user-create', user_data))
-#     self.assertEqual(response.status_code, 200)
-#     self.assertEqual(User.objects.count(), 1)
-#     user = User.objects.filter(username=self.c['username']).first()
-#     self.assertEqual(user.first_name, self.c.user_data['first_name'])
