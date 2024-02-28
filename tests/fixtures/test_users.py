cc4f721dd90bd7d95b3772174c8896ecf18ecfcb
Andrey Bogatyrev <t2way@yandex.ru>
2024-02-29 01:21:03 +0300

    add password1 & password2 to test_users.py so tests are not failed - test_user created, also update fixtures/test_users.py with TEST_USER_DATA_1&2

---
+++ tests/fixtures/test_users.py
@@ -0,0 +1,16 @@
+TEST_USER_DATA_1 = {
+    "username": "username_1",
+    "first_name": "first_name_1",
+    "last_name": "last_name_1",
+    "password1": "password1",
+    "password2": "password2",
+    "password": "password",
+}
+
+TEST_USER_DATA_2 = {
+    "username": "username_2",
+    "first_name": "first_name_2",
+    "last_name": "last_name_2",
+    "password1": "password2",
+    "password2": "password2",
+}
--- tests/test_users.py
+++ tests/test_users.py
@@ -7,26 +7,18 @@ django.setup()
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from django.urls import reverse
-from django.core.management import call_command
 
 
 class UserTestCase(TestCase):
 
-    # fixtures = ['new_test_user.json']
-
-    # @classmethod
-    # def setUpTestData(cls):
-    #     print("setUpTestData: Run once to set up non-modified data for all class methods.")
-
     def setUp(self):
         self.c = Client()
-        # call_command('loaddata', 'new_test_user.json')
         self.user_data = {
                     'first_name': 'Me',
                     'last_name': 'M',
                     'username': 'me',
-                    'password': 111,
-                    # 'password2': 'test_password2'
+                    'password1': 111,
+                    'password2': 111,
             }
 
     def test_create_user(self):
@@ -35,7 +27,3 @@ class UserTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         new_count = User.objects.count()
         self.assertEqual(old_count + 1, new_count)
-        # user = User.objects.filter(username=self.user_data["fields"]["username"]).first()
-        # self.assertIsNotNone(user)
-        # self.assertEqual(user.first_name, self.user_data['first_name'])
-        # self.assertEqual(user.last_name, self.user_data['last_name'])
