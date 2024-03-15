0dc75d936fc9473916042cccd12a7ebb253217cc
Andrey Bogatyrev <t2way@yandex.ru>
2024-03-15 21:23:27 +0300

    update test_users.py with new tests if create user such check: response 200, add to db, not add if same username, check correct some user data

--- tests/test_users.py
+++ tests/test_users.py
@@ -21,9 +21,28 @@ class UserTestCase(TestCase):
                     'password2': 111,
             }
 
-    def test_create_user(self):
-        old_count = User.objects.count()
+    def test_create_user_response_200(self):
         response = self.c.post(reverse('user:user-create'), self.user_data, follow=True)
         self.assertEqual(response.status_code, 200)
+    
+
+    def test_created_user_add_to_db(self):
+        old_count = User.objects.count()
+        self.c.post(reverse('user:user-create'), self.user_data, follow=True)
         new_count = User.objects.count()
         self.assertEqual(old_count + 1, new_count)
+
+
+    def test_check_for_not_create_user_with_same_username(self):
+        self.c.post(reverse('user:user-create'), self.user_data, follow=True)
+        users_count = User.objects.count()
+        self.c.post(reverse('user:user-create'), self.user_data, follow=True)
+        new_users_count = User.objects.count()
+        self.assertEqual(users_count, new_users_count)
+
+    
+    def test_create_user_with_correct_data(self):
+        self.c.post(reverse('user:user-create'), self.user_data, follow=True)
+        user = User.objects.filter(username=self.user_data['username']).first()
+        self.assertEqual(user.first_name, self.user_data['first_name'])
+        self.assertEqual(user.last_name, self.user_data['last_name'])
