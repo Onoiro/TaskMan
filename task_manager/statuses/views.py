b4eb52fed29a03c33c936e98972ea064425e50d3
Andrey Bogatyrev <t2way@yandex.ru>
2024-03-26 15:13:00 +0300

    make login required when update/delete statuses

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
@@ -8,10 +9,6 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Statuses
 from task_manager.statuses.forms import StatusForm
 
-# def index(request):
-#     return render(request, 'statuses/statuses.html', context={
-#         'status': 'START',
-#     })
 
 class StatusesListView(ListView):
     model = Statuses
@@ -26,16 +23,26 @@ class StatusesCreateView(SuccessMessageMixin, CreateView):
     success_message = _('Status created successfully')
 
 
-class StatusesUpdateView(SuccessMessageMixin, UpdateView):
+class StatusesPermissions(LoginRequiredMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.error(request, _('You are not authorized! Please login.'))
+            return super().dispatch(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
+
+
+class StatusesUpdateView(StatusesPermissions, SuccessMessageMixin, UpdateView):
     model = Statuses
     form_class = StatusForm
     template_name = 'statuses/statuses_update.html'
+    login_url = 'login'
     success_url = reverse_lazy('statuses:statuses-list')
     success_message = _('Status updated successfully')
 
 
-class StatusesDeleteView(SuccessMessageMixin, DeleteView):
+class StatusesDeleteView(StatusesPermissions, SuccessMessageMixin, DeleteView):
     model = Statuses
     template_name = 'statuses/statuses_delete.html'
+    login_url = 'login'
     success_url = reverse_lazy('statuses:statuses-list')
     success_message = _('Status deleted successfully')
