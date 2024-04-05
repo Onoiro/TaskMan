ce68caa6fb2cb52e27641371e35da4fe1b08e776
Andrey Bogatyrev <t2way@yandex.ru>
2024-04-05 18:58:11 +0300

    task CRUD now only authorized users

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,4 +1,6 @@
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import ListView, DetailView
@@ -7,14 +9,24 @@ from .models import Task
 from task_manager.tasks.forms import TaskForm
 
 
-class TasksListView(ListView):
+class TaskPermissions(LoginRequiredMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.error(request, _('You are not authorized! Please login.'))
+            return super().dispatch(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
+
+
+class TasksListView(TaskPermissions, ListView):
     model = Task
     template_name = 'tasks/tasks_list.html'
+    login_url = 'login'
 
 
-class TaskDetailView(DetailView):
+class TaskDetailView(TaskPermissions, DetailView):
     model = Task
     template_name = 'tasks/task_detail.html'
+    login_url = 'login'
 
 
 class TaskCreateView(SuccessMessageMixin, CreateView):
@@ -28,16 +40,18 @@ class TaskCreateView(SuccessMessageMixin, CreateView):
         return super().form_valid(form)
 
 
-class TaskUpdateView(SuccessMessageMixin, UpdateView):
+class TaskUpdateView(TaskPermissions, SuccessMessageMixin, UpdateView):
     model = Task
     form_class = TaskForm
     template_name = 'tasks/task_update.html'
+    login_url = 'login'
     success_url = reverse_lazy('tasks:tasks-list')
     success_message = _('Task updated successfully')
 
 
-class TaskDeleteView(SuccessMessageMixin, DeleteView):
+class TaskDeleteView(TaskPermissions, SuccessMessageMixin, DeleteView):
     model = Task
     template_name = 'tasks/task_delete.html'
+    login_url = 'login'
     success_url = reverse_lazy('tasks:tasks-list')
     success_message = _('Task deleted successfully')
