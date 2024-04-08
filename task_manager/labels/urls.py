eca2b62bde6cc1d96e481992bb4e64151e12651c
Andrey Bogatyrev <t2way@yandex.ru>
2024-04-08 14:19:01 +0300

    create app labels so update settings.py, urls.py, complit with labels/urls.py&views.py, now need to make labels/models.py&forms.py

---
+++ task_manager/labels/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+
+from .views import (
+    LabelsListView,
+    LabelsCreateView,
+    LabelsUpdateView,
+    LabelsDeleteView
+)
+
+
+app_name = 'labels'
+
+
+urlpatterns = [
+    path('',
+         LabelsListView.as_view(),
+         name='labels-list'),
+    path('create/',
+         LabelsCreateView.as_view(),
+         name='labels-create'),
+    path('<int:pk>/edit/',
+         LabelsUpdateView.as_view(),
+         name='labels-update'),
+    path('<int:pk>/delete/',
+         LabelsDeleteView.as_view(),
+         name='labels-delete'),
+]
+
---
+++ task_manager/labels/views.py
@@ -0,0 +1,44 @@
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Label
+from task_manager.labels.forms import LabelForm
+
+
+class LabelsPermissions(LoginRequiredMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.error(request, _('You are not authorized! Please login.'))
+            return super().dispatch(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
+
+
+class LabelsListView(LabelsPermissions, ListView):
+    model = Label
+    template_name = 'labels/labels_list.html'
+
+
+class LabelsCreateView(SuccessMessageMixin, CreateView):
+    form_class = LabelForm
+    template_name = 'labels/labels_create_form.html'
+    success_url = reverse_lazy('labels:labels-list')
+    success_message = _('Label created successfully')
+
+
+class LabelsUpdateView(LabelsPermissions, SuccessMessageMixin, UpdateView):
+    model = Label
+    form_class = LabelForm
+    template_name = 'labels/labels_update.html'
+    success_url = reverse_lazy('labels:labels-list')
+    success_message = _('Label updated successfully')
+
+
+class LabelsDeleteView(LabelsPermissions, SuccessMessageMixin, DeleteView):
+    model = Label
+    template_name = 'labels/labels_delete.html'
+    success_url = reverse_lazy('labels:labels-list')
+    success_message = _('Label deleted successfully')
