b0151be17e88abadf589a84aa10e6fe39a2fc0d3
Andrey Bogatyrev <t2way@yandex.ru>
2024-04-19 21:10:30 +0300

    improve filter.py to choose not many but only one label in form for filtering

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,8 +1,12 @@
 import django_filters
-from task_manager.tasks.models import Task
+from django import forms
+from task_manager.tasks.models import Task, Label
 
 
 class TaskFilter(django_filters.FilterSet):
+    labels = django_filters.ModelChoiceFilter(
+        queryset=Label.objects.all(), widget=forms.Select
+        )
 
     class Meta:
         model = Task
