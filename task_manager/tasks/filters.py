2830c212bac00d9d8c9c9fc13d29f7efed75a66c
Andrey Bogatyrev <t2way@yandex.ru>
2024-04-20 01:08:21 +0300

    make checkbox for show only current user's tasks - modify filters.py - left only {{ filter.form.as_p }} in task_filter.html

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,13 +1,42 @@
 import django_filters
 from django import forms
-from task_manager.tasks.models import Task, Label
+from task_manager.tasks.models import Task
+from task_manager.statuses.models import Status
+from task_manager.labels.models import Label
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class TaskFilter(django_filters.FilterSet):
-    labels = django_filters.ModelChoiceFilter(
-        queryset=Label.objects.all(), widget=forms.Select
+
+    status = django_filters.ModelChoiceFilter(
+        queryset=Status.objects.all(),
+        widget=forms.Select
+        )
+    
+    executor = django_filters.ModelChoiceFilter(
+        queryset=User.objects.all(),
+        widget=forms.Select
         )
 
+    labels = django_filters.ModelChoiceFilter(
+        queryset=Label.objects.all(),
+        widget=forms.Select
+        )
+    
+    own_tasks = django_filters.BooleanFilter(
+        field_name='author',
+        label=_('Just my tasks:'),
+        widget=forms.CheckboxInput(),
+        method='filter_own_tasks',
+    )
+    
     class Meta:
         model = Task
         fields = ['status', 'executor', 'labels']
+    
+    def filter_own_tasks(self, queryset, name, value):
+        user = self.request.user
+        if value:
+            return queryset.filter(author=user)
+        return queryset
