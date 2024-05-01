d180171927e784b74d2615ee4eab08192bca49a6
Andrey Bogatyrev <t2way@yandex.ru>
2024-05-01 19:11:18 +0300

    add status and labels cases to test_filter

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,4 +1,6 @@
 from task_manager.tasks.models import Task
+from task_manager.statuses.models import Status
+from task_manager.labels.models import Label
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from django.urls import reverse
@@ -12,11 +14,21 @@ class TaskTestCase(TestCase):
                 ]
 
     def setUp(self):
-        self.user = User.objects.get(username='me')
+        self.user = User.objects.get(username='he')
+        self.status = Status.objects.get(name='at work')
+        self.label = Label.objects.get(name='feature')
         self.c = Client()
         self.c.force_login(self.user)
-        self.response = self.c.get(reverse('tasks:tasks-list'))
-        self.filtered_tasks = Task.objects.filter(author=self.user)
+        self.filtered_tasks = Task.objects.filter(
+            executor=self.user,
+            status=self.status,
+            labels=self.label
+            )
+        self.response = self.c.get(reverse('tasks:tasks-list'),
+                                   {'executor': self.user.id,
+                                    'status': self.status.id,
+                                    'label': self.label.id
+                                    })
 
     def test_task_list_response_200(self):
         self.assertEqual(self.response.status_code, 200)
@@ -24,4 +36,5 @@ class TaskTestCase(TestCase):
     def test_filter_tasks_by_status_executor_label(self):
         filtered_task_ids = list(self.filtered_tasks.values_list('id', flat=True))
         response_task_ids = list(self.response.context['filter'].qs.values_list('id', flat=True))
+        print(filtered_task_ids, response_task_ids)
         self.assertListEqual(filtered_task_ids, response_task_ids)
