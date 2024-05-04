93a17664e364f33fc9dbc44dcb49ff97fccce362
Andrey Bogatyrev <t2way@yandex.ru>
2024-05-04 22:49:50 +0300

    modify statuses/forms.py for show translated label name & placeholder

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Status
+from django.utils.translation import gettext_lazy as _
 
 
 class StatusForm(forms.ModelForm):
@@ -7,3 +8,8 @@ class StatusForm(forms.ModelForm):
     class Meta:
         model = Status
         fields = ['name']
+    
+    name = forms.CharField(
+        label=_('Name'),
+        widget=forms.TextInput(attrs={'placeholder': _('Name')})
+    )
