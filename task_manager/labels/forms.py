81dca5e989e7217f0bb8d6f6333f919256fdb8ed
Andrey Bogatyrev <t2way@yandex.ru>
2024-05-04 22:58:25 +0300

    modify labels/forms.py for show translated label name & placeholder when labels/create

--- task_manager/labels/forms.py
+++ task_manager/labels/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from task_manager.labels.models import Label
+from django.utils.translation import gettext_lazy as _
 
 
 class LabelForm(forms.ModelForm):
@@ -7,3 +8,8 @@ class LabelForm(forms.ModelForm):
     class Meta:
         model = Label
         fields = ['name']
+    
+    name = forms.CharField(
+        label=_('Name'),
+        widget=forms.TextInput(attrs={'placeholder': _('Name')})
+    )
