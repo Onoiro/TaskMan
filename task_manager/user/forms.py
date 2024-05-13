2cbeba73232a7ec5de2591b23bcddba09ef92b97
Andrey Bogatyrev <t2way@yandex.ru>
2024-05-14 00:50:35 +0300

    try to fix bug probably in user/forms.py with not show message when password is less 3 chars, but shown message is still from help_text

--- task_manager/user/forms.py
+++ task_manager/user/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 from django.core import validators
+from django.core.exceptions import ValidationError
 
 
 class UserForm(forms.ModelForm):
@@ -26,33 +27,96 @@ class UserForm(forms.ModelForm):
             attrs={'placeholder': _('User name')})
     )
 
+    """ Works correct but if password less then 3 chars """
+    """ show only help_text message (not message specified in MinLengthValidator """
+    """(_("Your password is too short. It must contain at least 3 characters."))) """
+
     password1 = forms.CharField(
         label=_('Password'),
         widget=forms.PasswordInput(
             attrs={'placeholder': _('Password')}),
         help_text=_("Your password must contain at least 3 characters."),
         validators=[
-            validators.MinLengthValidator(
+                validators.MinLengthValidator(
                 limit_value=3,
                 message=_("Your password is too short. "
                           "It must contain at least 3 characters.")
             )])
+    
+    # password1 = forms.CharField(
+    #     label=_('Password'),
+    #     widget=forms.PasswordInput(
+    #         attrs={'placeholder': _('Password')}),
+    #     help_text=_("Your password must contain at least 3 characters."))
 
     password2 = forms.CharField(
         label=_('Confirm password'),
         widget=forms.PasswordInput(
             attrs={'placeholder': _('Confirm password')}),
         help_text=_("Please enter your password one more time"))
-
-    def clean_password2(self):
-        cleaned_data = self.cleaned_data
+    
+    def clean(self):
+        cleaned_data = super().clean()
         password1 = cleaned_data.get('password1')
         password2 = cleaned_data.get('password2')
         if password1 and password1 != password2:
             self.add_error(
-                'password2',
-                _('The entered passwords do not match.'))
+                "password2",
+                _("The entered passwords do not match."))
         return cleaned_data
+
+
+    """ Works correct but if password less then 3 chars """
+    """ show only help_text message and it's on top of all fields """
+    """ (not message specified in raise forms.ValidationError """
+    """(_("Your password is too short. It must contain at least 3 characters."))) """
+
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     password1 = cleaned_data.get('password1')
+    #     password2 = cleaned_data.get('password2')
+    #     if len(password1) < 3:
+    #         raise forms.ValidationError(_("Your password is too short. It must contain at least 3 characters."))
+    #     if password1 and password1 != password2:
+    #         raise ValidationError(_("The entered passwords do not match."))
+    #     return cleaned_data
+
+    
+    """ Works correct but if password less then 3 chars """
+    """ show only help_text message (not message specified in raise ValidationError) """
+
+    # def clean_password1(self):
+    #     password1 = self.cleaned_data.get('password1')
+    #     if len(password1) < 3:
+    #         raise forms.ValidationError(_("Your password is too short. It must contain at least 3 characters."))
+    #     return password1
+    
+    # def clean_password2(self):
+    #     password1 = self.cleaned_data.get('password1')
+    #     password2 = self.cleaned_data.get('password2')
+        
+    #     if password1 and password1 != password2:
+    #         raise ValidationError(_("The entered passwords do not match."))
+    #     return password2
+
+
+    """ Works correct but if password less then 3 chars """
+    """ show only help_text message (not message specified in add_error method) """
+
+    # def clean(self):
+    #     cleaned_data = self.cleaned_data
+    #     password1 = cleaned_data.get('password1')
+    #     password2 = cleaned_data.get('password2')
+    #     if len(password1) < 3:
+    #         self.add_error(
+    #             'password2',
+    #             _("Your password is too short. It must contain at least 3 characters."))
+    #     if password1 and password1 != password2:
+    #         self.add_error(
+    #             'password2',
+    #         _('The entered passwords do not match.')
+    #     )
+    #     return cleaned_data
 
     def save(self, commit=True):
         user = super().save(commit=False)
