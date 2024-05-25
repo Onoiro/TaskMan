e14be802687e9a363af5220491464e2a522e6f2c
Andrey Bogatyrev <t2way@yandex.ru>
2024-05-25 20:57:56 +0300

    add rollbar to project and configure it in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -71,7 +71,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 ]
+
+ROLLBAR = {
+    'access_token': '09ebf103d9cf4315a19721229b256b3a',
+    'environment': 'development' if DEBUG else 'production',
+    'code_version': '1.0',
+    'root': BASE_DIR,
+}
 
 ROOT_URLCONF = 'task_manager.urls'
 
