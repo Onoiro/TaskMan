6af20995ff5ebecb45bf7bf0ea5288e6ce539ad0
Andrey Bogatyrev <t2way@yandex.ru>
2024-05-28 14:31:23 +0300

    fix problem when try delete author or executor of task was raise error <Cannot delete some instances of model 'User' because they are referenced through protected foreign keys: 'Task.executor', 'Task.author'.> but now have another problem - when delete user no_author/no_executor raise error <Page not found>

--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,5 +1,4 @@
 from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib.auth.mixins import LoginRequiredMixin
 from task_manager.permissions import CustomPermissions
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
@@ -60,11 +59,11 @@ class UserDeleteView(UserPermissions, SuccessMessageMixin, DeleteView):
     success_url = reverse_lazy('user:user-list')
     success_message = _('User deleted successfully')
 
-    def delete(self, request, *args, **kwargs):
+    def form_valid(self, form):
         self.object = self.get_object()
         user_tasks_as_author = Task.objects.filter(author=self.object)
         user_tasks_as_executor = Task.objects.filter(executor=self.object)
         if user_tasks_as_author.exists() or user_tasks_as_executor.exists():
-            messages.error(request, _("Cannot delete a user because it is in use"))
+            messages.error(self.request, _("Cannot delete a user because it is in use"))
             return redirect('user:user-list')
-        return super().delete(request, *args, **kwargs)
+        return super().form_valid(form)
