fbe4a2fc9b208a8ad157eb4ba596f8ca2da4affd
Andrey Bogatyrev <t2way@yandex.ru>
2024-06-09 22:42:37 +0300

    make refactoring to task_manager/views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -9,16 +9,13 @@ from django.contrib import messages
 class IndexView(View):
 
     def get(self, request, *args, **kwargs):
-        hello_from_hexlet = _("Hello from Hexlet!")
-        coding_courses = _('Practical programming courses')
-        read_more = _("Read more")
-        exit = _("Exit")
-        return render(request, 'index.html',
-                      context={'hello_from_hexlet': hello_from_hexlet,
-                               'coding_courses': coding_courses,
-                               'read_more': read_more,
-                               'exit': exit,
-                               })
+        content = {
+            'hello_from_hexlet': _("Hello from Hexlet!"),
+            'coding_courses': _('Practical programming courses'),
+            'read_more': _("Read more"),
+            'exit': _("Exit")
+        }
+        return render(request, 'index.html', context=content)
 
 
 class UserLoginView(LoginView):
