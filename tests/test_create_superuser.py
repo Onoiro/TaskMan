0fe37e1048c263337da549bfb25bcd695f890cca
Andrey Bogatyrev <t2way@yandex.ru>
2024-06-22 15:33:11 +0300

    create & implement test_permissions.py, correct test_create_superuser.py for linter check

--- tests/test_create_superuser.py
+++ tests/test_create_superuser.py
@@ -3,15 +3,13 @@ from django.core.management import call_command
 from django.test import TestCase
 from io import StringIO
 
+
 class CreateSuperUserTestCase(TestCase):
     def test_create_superuser(self):
         out = StringIO()
         call_command('createsu', stdout=out)
         # self.assertIn('Superuser has been created.', out.getvalue())
-        
         existing_user_count = User.objects.filter(username='admin').count()
-        
         self.assertEqual(existing_user_count, 1)
-        
         admin = User.objects.get(username='admin')
         self.assertTrue(admin.is_superuser)
---
+++ tests/test_permissions.py
@@ -0,0 +1,37 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.messages import get_messages
+from django.utils.translation import gettext as _
+
+
+class PermissionsTestCase(TestCase):
+    fixtures = ["tests/fixtures/test_users.json",
+                "tests/fixtures/test_tasks.json",
+                "tests/fixtures/test_statuses.json",
+                "tests/fixtures/test_labels.json"]
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.get(username='me')
+        self.he = User.objects.get(username='he')
+
+    def test_custom_permissions_redirect_unauthenticated_user(self):
+        response = self.client.get(reverse(
+            'user:user-update', args=[self.he.id]), follow=True)
+        self.assertRedirects(response, reverse('login'))
+        messages = list(get_messages(response.wsgi_request))
+        self.assertGreater(len(messages), 0)
+        self.assertEqual(str(messages[0]),
+                         _('You are not authorized! Please login.'))
+
+    def test_user_permissions_can_not_modifying_other_user(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse(
+            'user:user-update', args=[self.he.id]), follow=True)
+        self.assertRedirects(response, reverse('user:user-list'))
+        messages = list(get_messages(response.wsgi_request))
+        self.assertGreater(len(messages), 0)
+        self.assertEqual(
+            str(messages[0]),
+            _("You don't have permissions to modify another user."))
