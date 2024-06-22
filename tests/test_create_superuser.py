a5d5350b9d3522648c0d42d0686ed9fe9ac052b3
Andrey Bogatyrev <t2way@yandex.ru>
2024-06-22 13:09:42 +0300

    add tests for createsu.py

---
+++ tests/test_create_superuser.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.core.management import call_command
+from django.test import TestCase
+from io import StringIO
+
+class CreateSuperUserTestCase(TestCase):
+    def test_create_superuser(self):
+        out = StringIO()
+        call_command('createsu', stdout=out)
+        # self.assertIn('Superuser has been created.', out.getvalue())
+        
+        existing_user_count = User.objects.filter(username='admin').count()
+        
+        self.assertEqual(existing_user_count, 1)
+        
+        admin = User.objects.get(username='admin')
+        self.assertTrue(admin.is_superuser)
