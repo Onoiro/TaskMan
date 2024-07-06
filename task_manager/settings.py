a849b49856f48c19d4dbb1ace7425745ebac6817
Andrey Bogatyrev <t2way@yandex.ru>
2024-07-06 18:32:55 +0300

    try to change default value of SECRET_KEY to KEY(that init earier) instead of 'KEY' because maybe its typing error(typo)

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 KEY = get_random_secret_key()
-SECRET_KEY = os.getenv('SECRET_KEY', 'KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
