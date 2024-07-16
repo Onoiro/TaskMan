0713978ae12f3b5546a2a70428e7419c49aceee9
Andrey Bogatyrev <t2way@yandex.ru>
2024-07-17 02:10:27 +0300

    update tasks/urls.py with correct path '<int:pk>/edit/' to <int:pk>/update/'

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
          TaskDetailView.as_view(), name='task-detail'),
     path('create/',
          TaskCreateView.as_view(), name='task-create'),
-    path('<int:pk>/edit/',
+    path('<int:pk>/update/',
          TaskUpdateView.as_view(), name='task-update'),
     path('<int:pk>/delete/',
          TaskDeleteView.as_view(), name='task-delete'),
