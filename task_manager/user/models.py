ea216a789fa56ececc81f6fdc6d3a7c94d7e7580
Abo <t2way@yandex.ru>
2024-11-24 21:14:45 +0300

    Added 'is_team_admin' field to UserForm and extended the User model to include team admin functionality
    - Introduced a new BooleanField 'is_team_admin' in UserForm to allow users to register as team admins.
    - Updated the user creation logic to handle the 'is_team_admin' field upon registration.
    - Modified the User model with a helper function to manage the team admin status.
    - Enhanced the `__str__` method of User to indicate admin status.

--- task_manager/user/models.py
+++ task_manager/user/models.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import User
+from django.db import models
 
 
+def is_team_admin(self):
+    is_team_admin = models.BooleanField(default=False)
+    return self.is_team_admin
+ 
 def user_str(self):
-    return f"{self.first_name} {self.last_name}"
+    return f"{self.first_name} {self.last_name} {'admin' if self.is_team_admin else ''}"
 
 
 User.__str__ = user_str
