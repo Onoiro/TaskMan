6932fd0f645189acfd9a4ec9d15fafb244cfda27
Abo <t2way@yandex.ru>
2025-03-28 22:06:16 +0300

    Add Team model to admin interface with custom display settings
    - implement TeamAdmin class with list display configurations, filtering, search capabilities,
    fieldsets organization, and queryset optimization through select_related

--- task_manager/teams/admin.py
+++ task_manager/teams/admin.py
@@ -1,3 +1,24 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Team
 
-# Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'team_admin', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'description', 'team_admin__username')
+    readonly_fields = ('created_at',)
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'team_admin')
+        }),
+        ('Дополнительная информация', {
+            'fields': ('description', 'created_at'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('team_admin')
+
+
+admin.site.register(Team, TeamAdmin)
