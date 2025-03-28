5f1bd58a7626bfcbea243bf52a1a497b2df9e27a
Abo <t2way@yandex.ru>
2025-03-28 21:37:49 +0300

    Add UserAdmin customization for team management
    - Register User model with custom UserAdmin class
    - Add team-related fields (is_team_admin, team) to admin interface
    - Customize display, filtering and search capabilities for team management
    - Uncomment admin imports and registration

--- task_manager/user/admin.py
+++ task_manager/user/admin.py
@@ -1,6 +1,20 @@
-# from django.contrib import admin
-# from .models import User
-# from django.contrib.auth.models import User
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
+class UserAdmin(BaseUserAdmin):
+    # Добавляем дополнительные поля в админку
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Команда', {'fields': ('is_team_admin', 'team')}),
+    )
+    
+    # дополнительные поля в списке пользователей
+    list_display = BaseUserAdmin.list_display + ('is_team_admin', 'team')
+    
+    # для фильтрации по этим полям
+    list_filter = BaseUserAdmin.list_filter + ('is_team_admin', 'team')
+    
+    # для поиска по этим полям
+    search_fields = BaseUserAdmin.search_fields + ('team__name',)
 
-# admin.site.register(User)
+admin.site.register(User, UserAdmin)
