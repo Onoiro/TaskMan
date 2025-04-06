298ec92ce03cd74ac004a0fbe0c6c8de4610b5b8
Abo <t2way@yandex.ru>
2025-04-07 00:23:18 +0300

    Implement team update functionality
    - Uncommented TeamUpdateView in urls.py
    - Implemented TeamUpdateView class
    - Added team update link in templates
    - Modified team details display in user_list.html
    - Improved team information display
    - Note: team_detail.html still exists but not currently used as team info is shown in user_list.html

--- task_manager/teams/urls.py
+++ task_manager/teams/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from task_manager.teams.views import (
     TeamCreateView,
     TeamDetailView,
-    # TeamUpdateView,
+    TeamUpdateView,
     # TeamDeleteView
 )
 
@@ -23,8 +23,8 @@ urlpatterns = [
          TeamCreateView.as_view(), name='team-create'),
     path('<int:pk>/detail/',
          TeamDetailView.as_view(), name='team-detail'),
-    # path('<int:pk>/update/',
-    #      TeamUpdateView.as_view(), name='team-update'),
+    path('<int:pk>/update/',
+         TeamUpdateView.as_view(), name='team-update'),
     # path('<int:pk>/delete/',
     #      TeamDeleteView.as_view(), name='team-delete'),
 ]
