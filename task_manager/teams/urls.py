dfb76ed2ed08471e3dbf14e43c62a7bb45e14abc
Abo <t2way@yandex.ru>
2025-04-12 11:09:09 +0300

    Add team deletion functionality
    - Implement TeamDeleteView class with team admin permissions
    - Add URL route for team deletion feature
    - Add team_delete.html template
    - Remove commented-out code and unused imports
    - Set up success message for team deletion confirmation

--- task_manager/teams/urls.py
+++ task_manager/teams/urls.py
@@ -3,20 +3,10 @@ from task_manager.teams.views import (
     TeamCreateView,
     TeamDetailView,
     TeamUpdateView,
-    # TeamDeleteView
+    TeamDeleteView,
 )
 
-# from task_manager.teams.views import (
-#     TeamListView,
-#     TeamDetailView,
-#     TeamCreateView,
-#     TeamUpdateView,
-#     TeamDeleteView
-# )
-
 app_name = 'teams'
-
-# urlpatterns = [path('', views.index),]
 
 urlpatterns = [
     path('create/',
@@ -25,18 +15,6 @@ urlpatterns = [
          TeamDetailView.as_view(), name='team-detail'),
     path('<int:pk>/update/',
          TeamUpdateView.as_view(), name='team-update'),
-    # path('<int:pk>/delete/',
-    #      TeamDeleteView.as_view(), name='team-delete'),
+    path('<int:pk>/delete/',
+         TeamDeleteView.as_view(), name='team-delete'),
 ]
-
-# urlpatterns = [
-#     path('', TeamListView.as_view(), name="teams-list"),
-#     path('<int:pk>',
-#          TeamDetailView.as_view(), name='team-detail'),
-#     path('create/',
-#          TeamCreateView.as_view(), name='team-create'),
-#     path('<int:pk>/update/',
-#          TeamUpdateView.as_view(), name='team-update'),
-#     path('<int:pk>/delete/',
-#          TeamDeleteView.as_view(), name='team-delete'),
-# ]
--- task_manager/teams/views.py
+++ task_manager/teams/views.py
@@ -51,21 +51,11 @@ class TeamUpdateView(SuccessMessageMixin,
     success_url = reverse_lazy('user:user-list')
 
 
-# class UserDeleteView(CustomPermissions,
-#                      UserPermissions,
-#                      SuccessMessageMixin,
-#                      DeleteView):
-#     model = User
-#     template_name = 'user/user_delete.html'
-#     success_url = reverse_lazy('user:user-list')
-#     success_message = _('User deleted successfully')
-
-#     def form_valid(self, form):
-#         self.object = self.get_object()
-#         user_tasks_as_author = Task.objects.filter(author=self.object)
-#         user_tasks_as_executor = Task.objects.filter(executor=self.object)
-#         if user_tasks_as_author.exists() or user_tasks_as_executor.exists():
-#             messages.error(self.request,
-#                            _("Cannot delete a user because it is in use"))
-#             return redirect('user:user-list')
-#         return super().form_valid(form)
+class TeamDeleteView(CustomPermissions,
+                     TeamAdminPermissions,
+                     SuccessMessageMixin,
+                     DeleteView):
+    model = Team
+    template_name = 'teams/team_delete.html'
+    success_url = reverse_lazy('user:user-list')
+    success_message = _('Team deleted successfully')
