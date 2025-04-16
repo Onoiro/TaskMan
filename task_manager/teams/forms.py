11df9abdfee93fe9a9219e02dcffbcfde1b13cc4
Abo <t2way@yandex.ru>
2025-04-17 00:05:27 +0300

    Added password protection mechanism for teams by implementing password fields in the Team model and form. The changes include:
    - Added a password field to the Team model with validation to ensure minimum length of 3 characters
    - Extended TeamForm with password and password confirmation fields
    - Implemented password validation logic including length verification and password matching
    - Modified the save method to properly hash and store the password
    - Re-enabled gettext translation support for internationalization
    
    This change allows teams to be protected with passwords, enhancing security by restricting team access to users who know the password.

--- task_manager/teams/forms.py
+++ task_manager/teams/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from task_manager.teams.models import Team
-# from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class TeamForm(forms.ModelForm):
@@ -9,8 +9,38 @@ class TeamForm(forms.ModelForm):
         model = Team
         fields = ['name', 'description']
 
+    password1 = forms.CharField(
+        required=True,
+        label=_('Password'),
+        widget=forms.PasswordInput(),
+        help_text=_("Your password must contain at least 3 characters."))
+
+    password2 = forms.CharField(
+        required=True,
+        label=_('Confirm password'),
+        widget=forms.PasswordInput(),
+        help_text=_("Please enter your password one more time"))
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if len(password1) < 3:
+            raise forms.ValidationError(_(
+                "Your password is too short."
+                " It must contain at least 3 characters."),
+                code='min_length')
+        return password1
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                _("The entered passwords do not match."))
+        return password2
+
     def save(self, commit=True):
         team = super().save(commit=False)
+        team.set_password(self.cleaned_data['password1'])
         if commit:
             team.save()
         return team
