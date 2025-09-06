b61e75baf9104ecd378e8d0af12711e223bfda07
Abo <t2way@yandex.ru>
2025-09-06 14:14:56 +0300

    Update for linter check pass and remove unneccessery code

--- task_manager/user/admin.py
+++ task_manager/user/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 from task_manager.teams.models import TeamMembership
 
+
 class TeamMembershipInline(admin.TabularInline):
     model = TeamMembership
     extra = 0
@@ -13,31 +14,27 @@ class TeamMembershipInline(admin.TabularInline):
 
 
 class UserAdmin(BaseUserAdmin):
-    # Обновляем list_display - убираем старые поля и добавляем новый метод
     list_display = (
-        'username', 
-        'email', 
-        'first_name', 
-        'last_name', 
+        'username',
+        'email',
+        'first_name',
+        'last_name',
         'is_staff',
-        'get_teams',  # новый метод для отображения команд
+        'get_teams',
         'date_joined'
     )
 
-    # Обновляем list_filter - убираем старые поля
     list_filter = (
-        'is_staff', 
-        'is_superuser', 
-        'is_active', 
+        'is_staff',
+        'is_superuser',
+        'is_active',
         'groups'
     )
 
-    # Добавляем inline для команд
     inlines = [TeamMembershipInline]
 
-    # Добавляем метод для отображения команд пользователя
     def get_teams(self, obj):
-        """Показывает список команд пользователя"""
+        """Show list of teams"""
         memberships = obj.team_memberships.select_related('team').all()
         if not memberships:
             return "No teams"
@@ -51,33 +48,13 @@ class UserAdmin(BaseUserAdmin):
         return ", ".join(teams_info)
 
     get_teams.short_description = 'Teams'
-    
-    # Если у вас есть fieldsets, обновите их тоже
-    fieldsets = BaseUserAdmin.fieldsets  # используем стандартные fieldsets
 
-    # Если нужно добавить информацию о командах в детальный просмотр:
+    fieldsets = BaseUserAdmin.fieldsets
+
+    # If need to add team info in team detail
     # fieldsets = BaseUserAdmin.fieldsets + (
-    #     ('Team Information', {'fields': ()}),  # Здесь можно добавить inline для команд
+    #     ('Team Information', {'fields': ()}),
     # )
 
 
 admin.site.register(User, UserAdmin)
-
-
-# class UserAdmin(BaseUserAdmin):
-#     # Добавляем дополнительные поля в админку
-#     fieldsets = BaseUserAdmin.fieldsets + (
-#         ('Команда', {'fields': ('is_team_admin', 'team')}),
-#     )
-
-#     # дополнительные поля в списке пользователей
-#     list_display = BaseUserAdmin.list_display + ('is_team_admin', 'team')
-
-#     # для фильтрации по этим полям
-#     list_filter = BaseUserAdmin.list_filter + ('is_team_admin', 'team')
-
-#     # для поиска по этим полям
-#     search_fields = BaseUserAdmin.search_fields + ('team__name',)
-
-
-# admin.site.register(User, UserAdmin)
