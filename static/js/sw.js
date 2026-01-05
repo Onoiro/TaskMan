f1f5bb09584cb04c72c6d6518065364e3594c09e
Abo <t2way@yandex.ru>
2026-01-05 23:28:18 +0300

    Remove unused icon files and update service worker
    - Delete old icon files from static directory
    - Update service worker cache version to v2
    - Add activate and fetch event handlers to service worker
    - Add cache busting version parameter to icon links
    - Set service worker scope to /static/

--- static/js/sw.js
+++ static/js/sw.js
@@ -1,13 +1,35 @@
-const CACHE_NAME = 'taskman-v1';
-  self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => cache.addAll([
-        '/',
-        '/static/css/custom.css',
-        '/static/icons/icon-192x192.png',
-        '/static/icons/icon-180x180.png',
-        '/static/images/favicon.ico'
-      ]))
-    );
-  });
-    
+const CACHE_NAME = 'taskman-v2';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll([
+      '/',
+      '/static/css/custom.css',
+      '/static/icons/icon-192x192.png',
+      '/static/icons/icon-180x180.png',
+      '/static/icons/icon-512x512.png',
+      '/static/images/favicon.ico'
+    ]))
+  );
+});
+
+// handler activate for remove old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
