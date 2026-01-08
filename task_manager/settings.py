75c03f8476ea1abae57bcb358e456cbd5db4c56f
Abo <t2way@yandex.ru>
2026-01-09 01:38:38 +0300

    Fix WhiteNoise static files storage backend Change CompressedManifestStaticFilesStorage to CompressedStaticFilesStorage for staticfiles configuration.

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -194,7 +194,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
