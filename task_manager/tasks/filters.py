7c4a2e5716f7760dde510470ca14efb4b006b8d7
Abo <t2way@yandex.ru>
2026-01-24 01:32:46 +0300

    Add exclude filters and improve UI styling
    - Add ability to exclude tasks by status, executor, labels, and own tasks
    - Import Q object for database queries
    - Add CSS classes to form widgets for better styling
    - Update filter template to show exclude checkboxes for each filter
    - Add filter_queryset method to handle exclude logic
    - Improve form widget appearance with form-select and form-check-input classes
    - Remove unnecessary comments in code

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,5 +1,6 @@
 import django_filters
 from django import forms
+from django.db.models import Q
 from task_manager.tasks.models import Task
 from task_manager.statuses.models import Status
 from task_manager.labels.models import Label
@@ -12,28 +13,28 @@ class TaskFilter(django_filters.FilterSet):
     status = django_filters.ModelChoiceFilter(
         queryset=Status.objects.all(),
         label=_('Status'),
-        widget=forms.Select,
+        widget=forms.Select(attrs={'class': 'form-select'}),
         label_suffix="",
     )
 
     executor = django_filters.ModelChoiceFilter(
         queryset=User.objects.all(),
         label=_('Executor'),
-        widget=forms.Select,
+        widget=forms.Select(attrs={'class': 'form-select'}),
         label_suffix="",
     )
 
     labels = django_filters.ModelChoiceFilter(
         queryset=Label.objects.all(),
         label=_('Label'),
-        widget=forms.Select,
+        widget=forms.Select(attrs={'class': 'form-select'}),
         label_suffix="",
     )
 
     self_tasks = django_filters.BooleanFilter(
         field_name='author',
         label=_('Just my tasks'),
-        widget=forms.CheckboxInput(),
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         method='filter_own_tasks',
         label_suffix="",
     )
@@ -74,7 +75,6 @@ class TaskFilter(django_filters.FilterSet):
         team = getattr(request, 'active_team', None)
 
         if team:
-            # filter by team
             team_users = User.objects.filter(
                 team_memberships__team=team
             ).distinct()
@@ -83,7 +83,6 @@ class TaskFilter(django_filters.FilterSet):
             self.filters['status'].queryset = Status.objects.filter(team=team)
             self.filters['labels'].queryset = Label.objects.filter(team=team)
         else:
-            # individual mode
             self.filters['executor'].queryset = User.objects.filter(pk=user.pk)
             self.filters['status'].queryset = Status.objects.filter(
                 creator=user,
@@ -99,3 +98,98 @@ class TaskFilter(django_filters.FilterSet):
         if value:
             return queryset.filter(author=user)
         return queryset
+
+    def filter_queryset(self, queryset):
+        """Override to handle exclude logic properly."""
+        # Get raw data from query params
+        data = self.form.data if self.form.is_bound else self.form.initial
+
+        # Helper to check if checkbox is checked
+        def is_excluded(param_name):
+            valid_values = ('on', 'true', '1', 'checked')
+            return param_name in data and data[param_name] in valid_values
+
+        user = self.request.user
+        team = getattr(self.request, 'active_team', None)
+
+        # Start with base queryset
+        if team:
+            qs = Task.objects.filter(team=team).order_by('-created_at')
+        else:
+            qs = Task.objects.filter(
+                author=user, team__isnull=True
+            ).order_by('-created_at')
+
+        # Get values from form (cleaned_data if valid, else initial)
+        status = None
+        if self.form.is_valid():
+            status = self.form.cleaned_data.get('status')
+        if status is None:
+            status = self.form.initial.get('status')
+
+        executor = None
+        if self.form.is_valid():
+            executor = self.form.cleaned_data.get('executor')
+        if executor is None:
+            executor = self.form.initial.get('executor')
+
+        labels = None
+        if self.form.is_valid():
+            labels = self.form.cleaned_data.get('labels')
+        if labels is None:
+            labels = self.form.initial.get('labels')
+
+        self_tasks = None
+        if self.form.is_valid():
+            self_tasks = self.form.cleaned_data.get('self_tasks')
+        if self_tasks is None:
+            self_tasks = self.form.initial.get('self_tasks')
+
+        created_after = None
+        if self.form.is_valid():
+            created_after = self.form.cleaned_data.get('created_after')
+        if created_after is None:
+            created_after = self.form.initial.get('created_after')
+
+        created_before = None
+        if self.form.is_valid():
+            created_before = self.form.cleaned_data.get('created_before')
+        if created_before is None:
+            created_before = self.form.initial.get('created_before')
+
+        # Apply filters with exclude logic
+        exclude_mode = is_excluded('status_exclude')
+        if status:
+            if exclude_mode:
+                qs = qs.filter(~Q(status=status))
+            else:
+                qs = qs.filter(status=status)
+
+        exclude_mode = is_excluded('executor_exclude')
+        if executor:
+            if exclude_mode:
+                qs = qs.filter(~Q(executor=executor))
+            else:
+                qs = qs.filter(executor=executor)
+
+        exclude_mode = is_excluded('labels_exclude')
+        if labels:
+            if exclude_mode:
+                qs = qs.filter(~Q(labels=labels))
+            else:
+                qs = qs.filter(labels=labels)
+
+        exclude_mode = is_excluded('self_tasks_exclude')
+        if self_tasks:
+            if exclude_mode:
+                qs = qs.filter(~Q(author=user))
+            else:
+                qs = qs.filter(author=user)
+
+        if created_after:
+            qs = qs.filter(created_at__gte=created_after)
+
+        if created_before:
+            qs = qs.filter(created_at__lte=created_before)
+
+        return qs
